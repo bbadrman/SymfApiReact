31a3532c5581f86bf0117261241d0f9db7417ba9
badrman <badr.bechtioui@gmail.com>
2022-06-18 20:34:45 +0100

    Mise en place du système d'authentification : l'entité User

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -44,6 +44,11 @@ class Customer
      */
     private $invoices;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="customers")
+     */
+    private $user;
+
     public function __construct()
     {
         $this->invoices = new ArrayCollection();
@@ -128,6 +133,18 @@ class Customer
                 $invoice->setCustomer(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
 
         return $this;
     }

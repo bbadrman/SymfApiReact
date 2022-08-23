493fb64624126ff613ce8b28b7f6318179b68d4c
badrman1983 <bbadrman@gmail.com>
2022-08-23 20:46:08 +0100

    in the branch i inject the script event for relaship customer with user in db

---
+++ src/Events/CustomerUserSubscriber.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Events;
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\Customer;
+use App\Entity\User;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Security;
+
+class CustomerUserSubscriber implements EventSubscriberInterface
+{
+
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::VIEW => ['setUserForCustomer', EventPriorities::PRE_VALIDATE]
+        ];
+    }
+
+    public function setUserForCustomer(ViewEvent $event)
+    {
+        $customer = $event->getControllerResult();
+
+        $method = $event->getRequest()->getMethod();
+
+        if ($customer instanceof Customer && $method === "POST") {
+            /** @var $user User */
+            $user = $this->security->getUser();
+            $customer->setUser($user);
+        }
+    }
+}

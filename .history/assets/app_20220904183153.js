6042aff4041700c9825139b415bdafeda548335e
badrman1983 <bbadrman@gmail.com>
2022-09-04 18:51:32 +0100

    10- Créer des routes protégées et inaccessibles aux utilisateurs anonymes

--- .history/assets/app_20220904183153.js
+++ .history/assets/app_20220904183153.js
@@ -25,24 +25,19 @@ import AuthAPI from "./services/authAPI";
 
 AuthAPI.setup();
 
-const PrivateRoute = ({path, isAuthenticated, component}) => 
-     isAuthenticated ? (<Route path={path} component={component} />) : (<Redirect to="/login" />);
-
 const App = () => {
     const [isAuthenticated, setIsAuthenticated] = useState(AuthAPI.isAuthenticated());
-
+   
     const NavbarWithRouter = withRouter(Navbar);
     return (<HashRouter>
         <NavbarWithRouter isAuthenticated={isAuthenticated} onLogout={setIsAuthenticated} />
         <main className="container pt-5">
             <Switch>
                 <Route path="/login" render={(props) => (
-                    <LoginPage
-                        onLogin={setIsAuthenticated} {...props}
-                    />)} />               
-                <PrivateRoute path="/invoices" isAuthenticated={ isAuthenticated } component={invoicesPage} />
-                <PrivateRoute path="/customers" isAuthenticated={ isAuthenticated } component={CustomersPage} />
-                
+                <LoginPage 
+                    onLogin={setIsAuthenticated} {...props}
+                    /> )}/>                <Route path="/invoices" component={invoicesPage} />
+                <Route path="/customers" component={CustomersPage} />
                 <Route path="/" component={HomePage} />
             </Switch>
         </main>
